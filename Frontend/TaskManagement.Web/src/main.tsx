669cb840427d705c26a67752759924e525c49e09
zeynepnazoztrk <zeynepnazoztrk7@gmail.com>
2026-08-19 15:02:19 +0300

    Frontend Görevleri - React ve PrimeReact Altyapısı

---
+++ Frontend/TaskManagement.Web/src/main.tsx
@@ -0,0 +1,15 @@
+import "primereact/resources/themes/md-dark-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
